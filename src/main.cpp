db7afb813b2cce488014544a0f81341f3ad0f8e4
tank <hnutank@163.com>
2016-03-13 19:58:16 +0800

    stack

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "common.h"
 #include "Vector.hpp"
+#include "Stack.hpp"
 using namespace std;
 #define LEN(a) sizeof(a)/sizeof(int)
 
@@ -16,11 +17,66 @@ void Vector_test()
     print(vi.begin(), vi.end());
 }
 
-int main(int argc, char *argv[])
+void Stack_test()
+{
+    cout<<"stack test\n";
+    Stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    cout<<st<<endl;
+    cout<<st.top()<<endl;
+    st.pop();
+    cout<<st.top()<<endl;
+    cout<<st<<endl;
+    cout<<"end stack test\n";
+}
+
+int polish(char *p)
+{
+    Stack<int> st;
+    char * q = p;
+    while( *q )
+    {
+        if( *q >= '0' && *q <= '9' )
+            st.push(*q - '0');
+        else
+        {
+            int a = st.top();
+            st.pop();
+            int b = st.top();
+            st.pop();
+            switch(*q)
+            {
+                case '+':
+                    st.push(a+b);
+                    break;
+                case '-':
+                    st.push(a-b);
+                    break;
+                case '*':
+                    st.push(a*b);
+                    break;
+                case '/':
+                    st.push(a/b);
+                    break;
+            }
+        }
+        ++q;
+    }
+
+    return st.top();
+}
+
+int main(int ,char **)
 {
     int a[] = {1,-23,3,4,-5,6,7,-8,9};
     cout<<max_nums_1(a, LEN(a))<<endl;
     cout<<max_nums_2(a, LEN(a))<<endl;;
     Vector_test();
+    Stack_test();
+
+    char *p = "6532+8*+3+*";
+    cout<<p<<" polish "<< polish(p)<<endl;
     return 0;
 }
