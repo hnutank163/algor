8317f780b9e28ba3528344acf907b7338c9409dd
hnutank163 <hnutank@163.com>
2016-03-16 11:23:58 +0800

    win32 prohect

--- src/Stack.hpp
+++ src/Stack.hpp
@@ -1,35 +1,52 @@
 #ifndef _STACK_H_
 #define _STACK_H_
 #include <iostream>
+
 template<typename Object>
 class Stack
 {
 public:
-    Stack(){
+    Stack()
+    {
         head = new Node();
         _size = 0;
     }
-    virtual ~Stack() { _delete(); }
+    virtual ~Stack()
+    {
+        _delete();
+    }
     void _delete()
     {
-        while(head)
-        {
-            delete head;
+        while(head) {
+            Node * old = head;
             head = head->next;
+            delete old;
         }
     }
     void push(const Object &obj)
     {
         ++ _size;
-        Node * node = new Node(obj, head);
-        head = node;
+        head = new Node(obj, head);
     }
 
-    Object & top(){return head->data;}
-    const Object & top() const { return head->data; }
-    bool empty(){return 0 == _size;}
-    size_t size(){ return _size; }
-    void pop(){
+    Object & top()
+    {
+        return head->data;
+    }
+    const Object & top() const
+    {
+        return head->data;
+    }
+    bool empty()
+    {
+        return 0 == _size;
+    }
+    size_t size()
+    {
+        return _size;
+    }
+    void pop()
+    {
         -- _size;
         Node * old =head;
         head = head->next;
@@ -39,21 +56,19 @@ public:
     friend std::ostream & operator<< (std::ostream &os, const Stack &st)
     {
         Node *node = st.head;
-        while(node->next)
-        {
+        while(node->next) {
             os<<node->data<<" ";
             node = node->next;
         }
         return os;
     }
 private:
-    struct Node
-    {
+    struct Node {
         Object data;
         Node * next;
         Node(Object _data=Object(), Node *_next =NULL):data(_data), next(_next)
         {
-            
+
         }
     };
 
